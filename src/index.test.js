2fb97774462d11347930ffdfb1a7220a1023bba9
Carrie Beesley <carrie@carriebeesley.com>
2019-11-06 16:38:29 +0000

    feat(tooling): upgrade to babel 7
    BREAKING CHANGE: Babel upgrade as yet untested

--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,14 @@
-describe("mv-support", () => {
-  it("works", () => {
-    expect(1 + 1).toBe(2);
+import SequelizeViewsSupport from './index';
+
+const sequelizeViewsSupport = new SequelizeViewsSupport('mydb', 'me', 'password', {
+  dialect: 'postgres',
+  host: 'example.com',
+  port: 5444,
+  pool: 8,
+});;
+
+describe("SequelizeViewsSupport", () => {
+  it("is an instance of SequelizeViewsSupport", () => {
+    expect(sequelizeViewsSupport).toBeInstanceOf(SequelizeViewsSupport);
   });
 });
