b204eaafaa06171ac2df8b09e01fff6311b6382f
Carrie Beesley <carrie@carriebeesley.com>
2020-03-23 08:46:33 +0000

    chore(deps): bump deps

--- src/SequelizeWithViews.ts
+++ src/SequelizeWithViews.ts
@@ -107,7 +107,7 @@ export class Sequelize extends SequelizeOrig.Sequelize {
     super.getQueryInterface();
 
     if (typeof this.queryInterface.dropMaterializedView !== 'function') {
-      this.queryInterface.dropMaterializedView = function(
+      this.queryInterface.dropMaterializedView = function (
         viewName: any
       ): Promise<[unknown[], unknown]> {
         const sql = `DROP MATERIALIZED VIEW IF EXISTS ${viewName}`;
@@ -116,7 +116,7 @@ export class Sequelize extends SequelizeOrig.Sequelize {
     }
 
     if (typeof this.queryInterface.createMaterializedView != 'function') {
-      this.queryInterface.createMaterializedView = function(
+      this.queryInterface.createMaterializedView = function (
         viewName: any,
         viewDefinition: string
       ): Promise<[unknown[], unknown]> {
@@ -127,7 +127,7 @@ export class Sequelize extends SequelizeOrig.Sequelize {
     }
 
     if (typeof this.queryInterface.refreshMaterializedView != 'function') {
-      this.queryInterface.refreshMaterializedView = function(
+      this.queryInterface.refreshMaterializedView = function (
         viewName: any
       ): Promise<[unknown[], unknown]> {
         return this.sequelize.query(`REFRESH MATERIALIZED VIEW ${viewName}`);
@@ -135,7 +135,7 @@ export class Sequelize extends SequelizeOrig.Sequelize {
     }
 
     if (typeof this.queryInterface.dropView !== 'function') {
-      this.queryInterface.dropView = function(
+      this.queryInterface.dropView = function (
         viewName: any,
         options: DropViewOptions = {}
       ): Promise<[unknown[], unknown]> {
@@ -147,7 +147,7 @@ export class Sequelize extends SequelizeOrig.Sequelize {
     }
 
     if (typeof this.queryInterface.createView != 'function') {
-      this.queryInterface.createView = function(
+      this.queryInterface.createView = function (
         viewName: any,
         viewDefinition: string
       ): Promise<[unknown[], unknown]> {
@@ -302,7 +302,7 @@ export class Sequelize extends SequelizeOrig.Sequelize {
    */
   getViews(): typeof Model[] {
     const models: typeof Model[] = [];
-    this.modelManager.forEachModel(function(model) {
+    this.modelManager.forEachModel(function (model) {
       if (isModelWithView(model)) models.push(model);
     });
     return models;
@@ -330,7 +330,7 @@ export class Sequelize extends SequelizeOrig.Sequelize {
    */
   getMaterializedViews(): typeof Model[] {
     const models: typeof Model[] = [];
-    this.modelManager.forEachModel(function(model) {
+    this.modelManager.forEachModel(function (model) {
       if (isModelWithMaterializedView(model)) models.push(model);
     });
     return models;
